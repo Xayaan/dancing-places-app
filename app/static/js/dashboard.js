918b47c5d68b3c271fd530acd9adc0e9ff2ad1c1
Tom <tom@hobofife.com>
2015-09-25 12:14:15 -0600

    HTML/CSS/JS Changes to "Add Dance" modal
    - Rename "Enter a Valid Address Here..." to "Street Address"
    - Reorder "Country" "City" "Address" "State/Region" "Location Name"
    - Under "Country", change "Waltz" to "Country Waltz"
    - Under "Latin" add "Samba"
    - Under "Club" remove "Other"
    - When mousing over the types of dance, shows the text selector, instead of a button mouse
    - Alphabetically order all dances, including the dance categories
    - Replace all "URL" text to "link"
    - Replace text with "Additional Information"
    - "Contact Person Email" should be "Contact Person or Email"

--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -2,15 +2,53 @@ function all_check(){
   $('.all-days-one').trigger('click');
 }
 
-var country_options = '<div class="well well-sm"><div class="checkbox"> <label> <input type="checkbox" name="country" value="line"> Line Dancing </label></div><div class="checkbox"> <label> <input type="checkbox" name="country" value="waltz"> Waltz </label></div><div class="checkbox"> <label> <input type="checkbox" name="country" value="texastwostep"> Texas Two Step </label></div><div class="checkbox"> <label> <input type="checkbox" name="country" value="twostep"> Two Step </label></div><div class="checkbox"> <label> <input type="checkbox" name="country" value="polka"> Polka </label></div></div>'
-var swing_options = '<div class="well well-sm"><div class="checkbox"> <label> <input type="checkbox" name="swing" value="east-coast"> East Coast </label></div><div class="checkbox"> <label> <input type="checkbox" name="swing" value="west-coast"> West Coast </label></div><div class="checkbox"> <label> <input type="checkbox" name="swing" value="hustle"> Hustle </label></div><div class="checkbox"> <label> <input type="checkbox" name="swing" value="lindyhop"> Lindy Hop </label></div><div class="checkbox"> <label> <input type="checkbox" name="swing" value="lindy"> Lindy </label></div></div>'
-var ballroom_options = '<div class="well well-sm"><div class="checkbox"> <label> <input type="checkbox" name="ballroom" value="waltz"> Waltz </label></div><div class="checkbox"> <label> <input type="checkbox" name="ballroom" value="tango"> Tango </label></div><div class="checkbox"> <label> <input type="checkbox" name="ballroom" value="foxtrot"> Foxtrot </label></div><div class="checkbox"> <label> <input type="checkbox" name="ballroom" value="foxy"> Foxy </label></div><div class="checkbox"> <label> <input type="checkbox" name="ballroom" value="atango"> Argentine Tango </label></div></div>'
-var latin_options = '<div class="well well-sm"><div class="checkbox"> <label> <input type="checkbox" name="latin" value="salsa"> Salsa </label></div><div class="checkbox"> <label> <input type="checkbox" name="latin" value="bachata"> Bachata </label></div><div class="checkbox"> <label> <input type="checkbox" name="latin" value="merengue"> Merengue </label></div><div class="checkbox"> <label> <input type="checkbox" name="latin" value="cumbia"> Cumbia </label></div><div class="checkbox"> <label> <input type="checkbox" name="latin" value="rumba"> Rumba </label></div><div class="checkbox"><label><input type="checkbox" name="latin" value="chacha">Cha-Cha</label></div><div class="checkbox"><label><input type="checkbox" name="latin" value="reggaeton">Reggaeton</label></div><div class="checkbox"><label><input type="checkbox" name="latin" value="kizomba">Kizomba</label></div><div class="checkbox"><label><input type="checkbox" name="latin" value="zouk">Zouk</label></div></div>'
-var club_options = '<div class="well well-sm"><div class="checkbox"> <label> <input type="checkbox" name="club" value="grinding"> Grinding </label></div><div class="checkbox"> <label> <input type="checkbox" name="club" value="other"> Other </label></div></div>'
+var ballroom_options = '<div class="well well-sm"> \
+<div class="checkbox"> <label> <input type="checkbox" name="ballroom" value="atango"> Argentine Tango </label></div> \
+<div class="checkbox"> <label> <input type="checkbox" name="ballroom" value="foxtrot"> Foxtrot </label></div> \
+<div class="checkbox"> <label> <input type="checkbox" name="ballroom" value="foxy"> Foxy </label></div> \
+<div class="checkbox"> <label> <input type="checkbox" name="ballroom" value="tango"> Tango </label></div> \
+<div class="checkbox"> <label><input type="checkbox" name="ballroom" value="waltz"> Waltz </label></div> \
+</div>'
+
+var country_options = '<div class="well well-sm"> \
+<div class="checkbox"> <label> <input type="checkbox" name="country" value="waltz"> Country Waltz </label></div> \
+<div class="checkbox"> <label> <input type="checkbox" name="country" value="line"> Line Dancing </label></div> \
+<div class="checkbox"> <label> <input type="checkbox" name="country" value="polka"> Polka </label></div> \
+<div class="checkbox"> <label> <input type="checkbox" name="country" value="texastwostep"> Texas Two Step </label></div> \
+<div class="checkbox"> <label> <input type="checkbox" name="country" value="twostep"> Two Step </label></div> \
+</div>'
+
+var swing_options = '<div class="well well-sm"> \
+<div class="checkbox"> <label> <input type="checkbox" name="swing" value="east-coast"> East Coast </label></div> \
+<div class="checkbox"> <label> <input type="checkbox" name="swing" value="hustle"> Hustle </label></div> \
+<div class="checkbox"> <label> <input type="checkbox" name="swing" value="lindy"> Lindy </label></div> \
+<div class="checkbox"> <label> <input type="checkbox" name="swing" value="lindyhop"> Lindy Hop </label></div> \
+<div class="checkbox"> <label> <input type="checkbox" name="swing" value="west-coast"> West Coast </label></div> \
+</div>'
+
+var latin_options = '<div class="well well-sm"> \
+<div class="checkbox"> <label> <input type="checkbox" name="latin" value="bachata"> Bachata </label></div> \
+<div class="checkbox"><label><input type="checkbox" name="latin" value="chacha">Cha-Cha</label></div> \
+<div class="checkbox"> <label> <input type="checkbox" name="latin" value="cumbia"> Cumbia </label></div> \
+<div class="checkbox"><label><input type="checkbox" name="latin" value="kizomba">Kizomba</label></div> \
+<div class="checkbox"> <label> <input type="checkbox" name="latin" value="merengue"> Merengue </label></div> \
+<div class="checkbox"><label><input type="checkbox" name="latin" value="reggaeton">Reggaeton</label></div> \
+<div class="checkbox"> <label> <input type="checkbox" name="latin" value="rumba"> Rumba </label></div> \
+<div class="checkbox"> <label> <input type="checkbox" name="latin" value="salsa"> Salsa </label></div> \
+<div class="checkbox"> <label> <input type="checkbox" name="latin" value="samba"> Samba </label></div> \
+<div class="checkbox"><label><input type="checkbox" name="latin" value="zouk">Zouk</label></div> \
+</div>'
+
+var club_options = '<div class="well well-sm"><div class="checkbox"> <label> <input type="checkbox" name="club" value="grinding"> Grinding </label></div></div>'
 
 $(document).on('click', '.type-upper h5', function(){
 	$('.type-upper h5').removeClass('active-upper');
 	$(this).addClass('active-upper');
+});
+
+$(document).on('click', '.ballroom-upper', function(){
+	$('.type-inner').empty();
+	$('.type-inner').append(ballroom_options);
 });
 
 $(document).on('click', '.country-upper', function(){
@@ -21,11 +59,6 @@ $(document).on('click', '.country-upper', function(){
 $(document).on('click', '.swing-upper', function(){
 	$('.type-inner').empty();
 	$('.type-inner').append(swing_options);
-});
-
-$(document).on('click', '.ballroom-upper', function(){
-	$('.type-inner').empty();
-	$('.type-inner').append(ballroom_options);
 });
 
 $(document).on('click', '.latin-upper', function(){
