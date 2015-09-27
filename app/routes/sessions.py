688101b83bd05386d42ada4dcbe100023829eba3
Tom <tom@hobofife.com>
2015-09-27 13:55:36 -0600

    Pull entry from database and populate form
    Still need to fix adding dance types and state to the databse. There isn't even a database field for state, and dance types are not saved correctly.

--- app/routes/sessions.py
+++ app/routes/sessions.py
@@ -102,5 +102,7 @@ def logout():
 
 @app.route("/dance/<int:entry_id>/edit", methods=['GET', 'POST'])
 def dance_edit(entry_id):
-    return render_template("edit_entry.html", entry_id=entry_id)
+    dance_entry = models.core.Dance.query.filter_by(id=entry_id).first()
+    return render_template("edit_entry.html", dance_entry=dance_entry)
+
 
